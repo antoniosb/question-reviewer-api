000f295fce7a2e572f987be5ead643f2a71d79b1
Hermógenes Ferreira <ferreira.hermogenes@gmail.com>
2018-06-05 10:24:22 -0300

    feat(ajustar-cors-options) Ajustar Options e CORS para acesso do frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  match '*path', via: [:options], to:  lambda {|_| [204, {'Access-Control-Allow-Headers' => "Origin, Content-Type, Accept, Authorization, Token", 'Access-Control-Allow-Origin' => "*", 'Content-Type' => 'text/plain'}, []]}
   post 'auth/token' => 'auth#token'
   resources :users, only: [:create]
   resources :questions do
